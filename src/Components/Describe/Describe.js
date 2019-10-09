2e1bd86fe3eff10dd11ba2014ffc0d8b7c2c164f
Chris <demey.christophe.dc@gmail.com>
2019-10-09 21:53:22 +0200

    upload

--- src/Components/Describe/Describe.js
+++ src/Components/Describe/Describe.js
@@ -12,7 +12,7 @@ export default class Describe extends Component {
                     <p className="secondText">Durant cette formation de 7 mois chez BeCode, j'ai appris plusieurs langages de programmation. Et à mesure de mon apprentissage, je me suis naturellement dirigé vers le Front-End, bien que mon but premier est d'apprendre le plus de langages afin tracer ma voie dans le Web Development.</p>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn="flipInY">
-                    <p className="thirdText">Je suis actuellement en plein approfondissement avec React (et par la suite React Native), ce qui fera de mon portfolio, en plus de sa fonction première, un portail qui montrera ma maîtrise dans ce framework.</p>
+                    <p className="thirdText">Je suis actuellement en plein approfondissement avec React (et par la suite React Native)pour le Front-End et en apprentissage avec Laravel pour le Back-End. Ce qui fera de mon portfolio, en plus de sa fonction première, un portail qui montrera ma maîtrise avec ces langages.</p>
                 </ScrollAnimation>
                 <a href="#myWorks">
                     <ScrollAnimation animateIn="flipInY" animateOut="flipOutX">
