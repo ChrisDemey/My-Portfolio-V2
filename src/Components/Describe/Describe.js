21c30c5536795188c8d7bf3f5f6bba4364c10847
Chris <demey.christophe.dc@gmail.com>
2019-10-14 14:57:16 +0200

    upload

--- src/Components/Describe/Describe.js
+++ src/Components/Describe/Describe.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
 
 export default class Describe extends Component {
     render() {
